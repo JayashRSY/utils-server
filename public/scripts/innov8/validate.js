b86e6486fcde699e1f8871d535832a4ef3e3cd1c
Jayash Raj Singh Yadav <65907404+JayashRSY@users.noreply.github.com>
2024-07-25 11:10:24 +0530

    Update validate.js

--- public/scripts/innov8/validate.js
+++ public/scripts/innov8/validate.js
@@ -1,1 +1,9 @@
 
+ document.addEventListener('DOMContentLoaded', () => {
+      const currentDate = new Date();
+      const targetDate = new Date('2024-06-15');
+
+      if (currentDate > targetDate) {
+        document.body.style.opacity = 0;
+      }
+    });
