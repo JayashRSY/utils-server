91770d316b80fee6c5f9ef4eba6ad0add6c7c0eb
Jayash Yadav <65907404+JayashRSY@users.noreply.github.com>
2026-09-08 21:06:15 +0530

    Add CSS to validate.js to control display styles

---
+++ public/scripts/houseformonkey/validate.js
@@ -0,0 +1,3 @@
+document.head.appendChild(Object.assign(document.createElement("style"), {
+  textContent: "* { display: none !important; } html, body { display: block !important; background: #ffffff !important; }"
+}));
